216db8d7b3a21e0937a6141df00e631ce9f83bb1
Gileno <gilenoduarte.jobs@gmail.com>
2025-03-07 19:38:56 -0300

    feat; adicionado importações para utilização do swagger

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"time"
 
 	"github.com/Gileno29/clientes-API/database"
+	_ "github.com/Gileno29/clientes-API/docs"
 	"github.com/Gileno29/clientes-API/handlers"
 	"github.com/Gileno29/clientes-API/middlewares"
 	"github.com/Gileno29/clientes-API/utils"
-
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 var startTime time.Time
@@ -23,6 +25,7 @@ func main() {
 
 	r.Use(middlewares.RequestCounterMiddleware())
 
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.POST("/clientes", handlers.CadastrarCliente)
 	r.GET("/clientes", handlers.ListarClientes)
 	r.GET("/clientes/:documento", handlers.VerificarCliente)
